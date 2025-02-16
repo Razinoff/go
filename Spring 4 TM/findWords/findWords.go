c2ddb55b8c6034401dea535ff65f5e5d352a4d05
Razinoff <razin230905@gmail.com>
2025-02-17 05:15:23 +0600

    cs2

--- Spring 4 TM/findWords/findWords.go
+++ Spring 4 TM/findWords/findWords.go
@@ -7,10 +7,33 @@ func findWords(words []string) []string {
 	Middleline := "asdfghjkl"
 	Lowerline := "zxcvbnm"
 	var result []string
+	var flag bool
 	for _, value := range words {
-		if strings.Contains(Topline, strings.ToLower(value)) || strings.Contains(Middleline, strings.ToLower(value)) || strings.Contains(Lowerline, strings.ToLower(value)) {
+		valuelow := strings.ToLower(value)
+		valuelowarr := strings.Split(valuelow, "")
+		if strings.Contains(Topline, valuelowarr[0]) {
+			flag = findWord(valuelowarr, Topline)
+		} else if strings.Contains(Middleline, valuelowarr[0]) {
+			flag = findWord(valuelowarr, Middleline)
+		} else if strings.Contains(Lowerline, valuelowarr[0]) {
+			flag = findWord(valuelowarr, Lowerline)
+		} else {
+			flag = false
+		}
+		if flag {
 			result = append(result, value)
 		}
+
 	}
+
 	return result
+}
+func findWord(strword []string, strline string) bool {
+	for _, value := range strword {
+		if strings.Contains(strline, value) {
+		} else {
+			return false
+		}
+	}
+	return true
 }
